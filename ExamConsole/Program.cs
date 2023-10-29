fdbadc639a4f127dab3ed891d72deba0c594d30c
Maksim <bobreczovm@inbox.ru>
2023-10-29 12:18:36 +0300

    Добавили консоль

---
+++ ExamConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
